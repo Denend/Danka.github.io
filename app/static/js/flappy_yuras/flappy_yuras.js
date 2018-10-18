ad8680e2b1827d7e8aed216c0076cf6853f897fa
Danylo Soloviov <41167337+Denend@users.noreply.github.com>
2018-10-18 20:23:09 +0300

    flappy uras delete spaces

--- app/static/js/flappy_yuras/flappy_yuras.js
+++ app/static/js/flappy_yuras/flappy_yuras.js
@@ -30,25 +30,23 @@ var count = 0;
 var counter_t = 0;
 var AnimSpeed = 0;
 var check_click = 0;
-//Позиция юры
-// при нажатии на кнопку
 document.addEventListener('DOMContentLoaded', function(){
 document.addEventListener("keydown", moveUp);
 document.addEventListener("touchend", moveUp);
  $('#canvas').bind('click touchend', function(e) {
  e.preventDefault();
  $(this).click();
-});
+ });
 button_run = document.getElementById("poigrat")
 button_run.onclick = run_game;
 
 });
 function moveUp(){
   if(check_click == 0){
-    return;
+   return;
   };
 yPoz -= 35;
-fly.play();
+ fly.play();
 };
 
 
@@ -59,8 +57,8 @@ shyshki[0] = {
 }
 var kosyaki = [];
 kosyaki[0] = {
-x: canvas.width,
-y: 0
+ x: canvas.width,
+ y: 0
 }
 var xPoz = 10;
 var yPoz = 150;
@@ -69,55 +67,36 @@ var graviton = 0;
 function throttle(fn, threshhold, scope) {
   threshhold || (threshhold = 250); //250
   var last,
-      deferTimer;
+  deferTimer;
   return function () {
     var context = scope || this;
 
     var now = +new Date,
-        args = arguments;
+    args = arguments;
     if (last && now < last + threshhold) {
-      // hold on to it
       clearTimeout(deferTimer);
       deferTimer = setTimeout(function () {
-        last = now;
-        fn.apply(context, args);
+       last = now;
+       fn.apply(context, args);
       }, threshhold);
-    } else {
-      last = now;
-      fn.apply(context, args);
-    }
-  };
-}
+      } else {
+       last = now;
+       fn.apply(context, args);
+      }
+   };
+ }
 
   var f = function score_withdelay(){
-    //var k = Math.floor(Math.random() * 2);
-  //  if(k==0){
-      score_aud.play();
-      ura=ura_high;
-    //} else
-
-};
+    score_aud.play();
+    ura=ura_high;
+  };
 var run_interval = throttle(f, 100);
-
-// пишем таймер
-/*var id = setInterval("time_count()",1000);
-
 function time_count(){
-counter_time++
-if(counter_time ==30){
-  AnimSpeed++
+  counter_t++
+  if(counter_t ==30){
+    AnimSpeed++
+  };
 };
-
-};   */
-function time_count(){
-counter_t++
-//alert("counter_time")
-if(counter_t ==30){
-  AnimSpeed++
-};
-
-};
-
 function run_game(){
   AnimSpeed = 1;
   graviton = 1.5;
@@ -125,21 +104,14 @@ function run_game(){
 
   setInterval("time_count()",1000);
   button_run.disabled = true;
-
-
 };
 
 var funcAnimate = setInterval(function narisowat(){
-
-
 context.drawImage(bg, 0, 0);
 for(i=0;i<kosyaki.length;i++){
 context.drawImage(kosoj_up, kosyaki[i].x , kosyaki[i].y );
-// высота 272
 context.drawImage(kosoj_bot, kosyaki[i].x, kosyaki[i].y + kosoj_up.height + gap);
-// высота 378 и они неизменные просто указываются координаты а остаток обрезается канвасом
 context.drawImage(shishlo, shyshki[i].x, shyshki[i].y);
-//if(counter_time < 10){
 shyshki[i].x = shyshki[i].x - AnimSpeed;
 kosyaki[i].x = kosyaki[i].x - AnimSpeed;
 if(AnimSpeed == 1 && kosyaki[i].x==80){
@@ -160,13 +132,10 @@ if(AnimSpeed == 1 && kosyaki[i].x==80){
   shyshki.push({
     x: canvas.width + 110,
     y: Math.floor(Math.random()*(canvas.width - 50))
-
   })
-
 };
-
 if(xPoz + ura.width >= shyshki[i].x && xPoz <= shyshki[i].x + shishlo.width && yPoz + ura.height/2 <=shyshki[i].y + shishlo.height && yPoz + ura.height/2 >= shyshki[i].y) {
-  count += 1;
+ count += 1;
  shyshki[i].y = -300
  run_interval();
 }
@@ -175,11 +144,10 @@ if (yPoz <= 0 && counter_t == 25  && count < 50){
 
 };
 if(xPoz + ura.width - 5>= kosyaki[i].x + 5 && xPoz <= kosyaki[i].x + kosoj_up.width - 5 && (yPoz<= kosyaki[i].y +kosoj_up.height||yPoz + ura.height - 5>=kosyaki[i].y + kosoj_up.height + gap) || yPoz<=-5 || yPoz + ura.height >= canvas.height - fg.height + 10){
-//location.reload();
 result.innerHTML = "Previous result = " + count;
 
 if(count > 0) {
-var obj_send2 = {
+ var obj_send2 = {
   count
 }
 var string_result = JSON.stringify(obj_send2);
@@ -200,9 +168,8 @@ kosyaki[i].x = kosyaki[0].x;
 shyshki[i].x = shyshki[0].x;
 xPoz = 10;
 yPoz = 150;
-//history.go(0)   // перезагружает всю страницу ваще
 } else {
- scoreLose.play();
+  scoreLose.play();
   counter_t = 0;
   AnimSpeed = 1;
   count = 0;
@@ -217,14 +184,10 @@ yPoz = 150;
 };
 };
 
-context.drawImage(fg, 0 , cvs.height-fg.height);
-// cvs 512,288   fg 118
+context.drawImage(fg, 0 , cvs.height-fg.height)
 context.drawImage(ura, xPoz , yPoz);
 yPoz += graviton;
 context.fillStyle = "#000";
 context.font = "24px Verdana"
 context.fillText("Score:" + count, 90,cvs.height - 50)
-//requestAnimationFrame(narisowat);
 }, 1000/60);
-
-//shishlo.onload = funcAnimate;
